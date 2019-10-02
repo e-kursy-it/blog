598c5c6125e8fd66a7d73cd3f5ac7b3e147c9661
Marek Bedkowski <marek@bedkowski.pl>
2019-10-02 20:36:41 +0200

    Update deploy script.

--- nettyintroduction/src/main/resources/deployment_scripts/deploy.sh
+++ nettyintroduction/src/main/resources/deployment_scripts/deploy.sh
@@ -4,6 +4,10 @@ LOCAL_IPV4=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 
 sudo useradd nettyapp
 
+sudo chown nettyapp:nettyapp /opt/nettyapp/run.sh
+
+sudo chmod a+x /opt/nettyapp/run.sh
+
 read -r -d '' SERVICE << SERVICE
 [Unit]
 Description=Netty Http Server Example
